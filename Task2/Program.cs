abdbaaf59132c511db782084170e290ffe314613
Денис Степаненко <denis.stepanenko@hotmail.com>
2022-08-11 09:36:15 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое чило");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите второе чило");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Результат");
+            
+            if (a > b)
+                Console.WriteLine(a);
+            else
+                Console.WriteLine(b);
+
+            Console.ReadLine();
+        }
+    }
+}
